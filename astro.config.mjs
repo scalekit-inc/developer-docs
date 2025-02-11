fc32048397a17e2e6cddb3ddbd24e7c51225cf1f
saif-at-scalekit <saif.shaik@scalekit.com>
2025-02-11 21:52:50 +0530

    Code-expressive (#2)
    * Remove empty line in Astro configuration
    
    * Inline Starlight configuration and update Expressive Code dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,41 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import { starlightConfig } from './src/starlight.config.js';
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight(starlightConfig), react(), vue()],
-
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      expressiveCode: {
+        themes: ['material-theme', 'github-light'],
+      },
+      customCss: [
+        '@fontsource-variable/plus-jakarta-sans',
+        './src/styles/custom.css',
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            {
+              label: 'Example Guide',
+              slug: 'guides/example',
+              collapsed: false,
+            },
+            { label: 'Example Guide 2', slug: 'guides/example-2' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+    react(),
+    vue(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
