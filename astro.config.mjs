2f994849de8f03b6bad239d96e3b73a666f9196c
saif-at-scalekit <saif.shaik@scalekit.com>
2025-02-13 23:51:06 +0530

    Add Scalar API Reference and Vue plugin to project dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,61 +5,60 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://scalekit.com', // creates a sitemap for robots.txt
-  integrations: [
-    starlight({
-      title: 'My Docs',
-      logo: {
-        src: '@/assets/logo-test.png',
-        replacesTitle: true,
+  integrations: [starlight({
+    title: 'My Docs',
+    logo: {
+      src: '@/assets/logo-test.png',
+      replacesTitle: true,
+    },
+    defaultLocale: 'en',
+    social: {
+      github: 'https://github.com/scalekit-inc',
+      linkedin: 'https://www.linkedin.com/company/scalekit-inc/',
+    },
+    // allows for open contributions to the docs
+    editLink: {
+      baseUrl:
+        'https://github.com/scalekit-inc/scalekit-docs/edit/main/src/content/docs/',
+    },
+    expressiveCode: {
+      themes: ['material-theme', 'github-light'],
+      useStarlightDarkModeSwitch: true,
+    },
+    customCss: [
+      '@fontsource-variable/plus-jakarta-sans',
+      '@fontsource-variable/space-grotesk',
+      './src/styles/custom.css',
+    ],
+    sidebar: [
+      {
+        label: 'Guides',
+        items: [
+          {
+            label: 'Components',
+            slug: 'guides/components',
+            collapsed: false,
+          },
+          { label: 'Example Guide 2', slug: 'guides/example' },
+          { label: 'Plugins', slug: 'guides/plugins' },
+        ],
       },
-      defaultLocale: 'en',
-      social: {
-        github: 'https://github.com/scalekit-inc',
-        linkedin: 'https://www.linkedin.com/company/scalekit-inc/',
+      {
+        label: 'Reference',
+        autogenerate: { directory: 'reference' },
       },
-      // allows for open contributions to the docs
-      editLink: {
-        baseUrl:
-          'https://github.com/scalekit-inc/scalekit-docs/edit/main/src/content/docs/',
+      {
+        label: 'Scalekit',
+        link: 'https://www.scalekit.com',
+        attrs: { target: '_blank', style: 'font-style: italic' },
       },
-      expressiveCode: {
-        themes: ['material-theme', 'github-light'],
-        useStarlightDarkModeSwitch: true,
-      },
-      customCss: [
-        '@fontsource-variable/plus-jakarta-sans',
-        '@fontsource-variable/space-grotesk',
-        './src/styles/custom.css',
-      ],
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            {
-              label: 'Components',
-              slug: 'guides/components',
-              collapsed: false,
-            },
-            { label: 'Example Guide 2', slug: 'guides/example' },
-            { label: 'Plugins', slug: 'guides/plugins' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-        {
-          label: 'Scalekit',
-          link: 'https://www.scalekit.com',
-          attrs: { target: '_blank', style: 'font-style: italic' },
-        },
-      ],
-    }),
-    react(),
-  ],
+    ],
+  }), react(), vue()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
