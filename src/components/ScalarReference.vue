d9f2bbe593aee4bec17c8bf8e3b7cff13af4b4dd
saif-at-scalekit <saif.shaik@scalekit.com>
2025-02-18 18:12:46 +0530

    Configure Scalar Reference component with favicon and client button settings
    - Add custom favicon URL
    - Hide client button by default

--- src/components/ScalarReference.vue
+++ src/components/ScalarReference.vue
@@ -54,10 +54,13 @@ import '@fontsource-variable/plus-jakarta-sans';
           'nsurlsession',
           'undici',
         ],
+        favicon:
+          'https://cdn.prod.website-files.com/65b87d98fa638289e10b8f61/67ab35acf2f180e0fe0a94a8_S%20Icon%20-%20Black%20-%2032x32.png',
         defaultHttpClient: {
           targetKey: 'node',
           clientKey: 'axios',
         },
+        hideClientButton: true,
       }"
     />
   </div>
