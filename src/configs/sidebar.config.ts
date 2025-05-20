6770f8339793542c8eb953b949847bfedfbd869f
saif-at-scalekit <saif.shaik@scalekit.com>
2025-05-20 22:16:53 +0530

    Refactor sidebar configuration and update guide documentation
    - Renamed the 'Guides' section to 'Foundations' and streamlined the items for better clarity.
    - Added new guides for 'Automatically assign roles' and 'Redirects' while removing outdated entries.
    - Updated sidebar links for 'Resources' and 'Reference' sections to enhance navigation.
    - Removed the Go, Node.js, Python, and Java SDK documentation files to declutter the reference section.

--- src/configs/sidebar.config.ts
+++ src/configs/sidebar.config.ts
@@ -39,138 +39,42 @@ export const sidebar = [
         ],
       },
       {
-        label: 'Guides',
+        label: 'Foundations',
         items: [
-          {
-            label: 'How to',
-            items: [
-              'home/guides/test-sso',
-              'home/guides/custom-domain',
-              'home/guides/idp-init-sso',
-              'home/guides/okta-sso-test',
-              {
-                label: 'Integrate with',
-                items: [
-                  'integrations/auth-systems/auth0',
-                  'integrations/auth-systems/firebase',
-                  'integrations/auth-systems/aws-cognito',
-                ],
-              },
-            ],
-          },
-          {
-            label: 'Concepts',
-            items: [
-              'home/guides/custom-domain',
-              'home/guides/idtoken-claims',
-              'home/guides/redirects',
-              'home/guides/sso-basics',
-              'home/guides/sso-user-attributes',
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  /* Remove this sidebar for migration release
-  {
-    label: 'Manual',
-    id: 'manual',
-    link: '/manual/getting-started/',
-    icon: 'open-book',
-    items: [
-      {
-        label: 'Getting Started',
-        items: [
-          {
-            label: 'Installation',
-            link: 'manual/getting-started/installation',
-          },
-          {
-            label: 'Sign in users',
-            link: '/manual/getting-started/authenticate-users',
-          },
-          {
-            label: 'Manage sessions',
-            link: '/manual/getting-started/manage-user-sessions',
-          },
-          {
-            label: 'Manage users',
-            link: '/manual/getting-started/manage-users',
-          },
-          {
-            label: 'Sign out users',
-            link: '/manual/getting-started/sign-out',
-          },
-          {
-            label: 'Code examples',
-            link: '/manual/support/code-examples-catalog',
-          },
-        ],
-      },
-      {
-        label: 'Quickstarts',
-        collapsed: false,
-        items: [
-          'manual/social-logins-quickstart',
-          'manual/sso-quickstart',
-          'manual/scim-quickstart',
-          'manual/quickstarts/m2m',
-          {
-            label: 'Launch',
-            link: '/manual/fundamentals/launch-checklist',
-          },
-        ],
-      },
-      {
-        label: 'Fundamentals',
-        items: [
-          {
-            label: 'Admin Portal',
-            link: '/manual/fundamentals/admin-portal',
-          },
-          {
-            label: 'IdP Simulator',
-            link: '/manual/fundamentals/test-sso-integration',
-          },
-          {
-            label: 'Automatically assign roles',
-            link: '/manual/fundamentals/group-based-role-assignment',
-          },
-          {
-            label: 'More product guides ↗',
-            link: '/guides/',
-          },
+          'home/guides/custom-domain',
+          'home/guides/redirects',
+          'home/guides/automatically-assign-roles',
+          'home/guides/test-sso',
+          'home/guides/sso-user-attributes',
         ],
       },
       {
         label: 'Resources',
         items: [
           {
+            label: 'API reference',
+            link: '/apis',
+          },
+          {
             label: 'API collections',
             link: 'https://github.com/scalekit-developers/api-collections',
           },
           {
-            label: 'Release notes',
-            link: 'https://www.scalekit.com/product-updates',
-          },
-          {
-            label: 'Chat with us!',
-            link: '/manual/support/contact-us',
+            label: 'Chat with us',
+            link: 'home/contact-us',
           },
           {
             label: 'Glossary',
-            link: '/manual/support/glossary',
+            link: '/reference/glossary',
           },
           {
-            label: 'Status Page',
+            label: 'Status',
             link: 'https://scalekit.statuspage.io/',
           },
         ],
       },
     ],
   },
-  */
   {
     label: 'Reference',
     id: 'reference',
@@ -178,83 +82,74 @@ export const sidebar = [
     icon: 'document',
     items: [
       {
-        label: 'API',
-        items: [
-          {
-            label: 'REST APIs ↗',
-            link: '/apis',
-          },
-          {
-            label: 'API Authentication',
-            link: '/reference/api-catalog/authenticate-scalekit-api',
-          },
-          {
-            label: 'User authentication',
-            link: '/reference/concepts/auth-endpoints',
-          },
-          {
-            label: 'UI events',
-            link: '/reference/api-catalog/ui-events',
-          },
-        ],
+        label: 'REST APIs ↗',
+        link: '/apis',
       },
       {
-        label: 'Concepts',
-        items: [
-          {
-            label: 'Custom identifiers & data',
-            link: '/reference/concepts/external-ids-and-metadata',
-          },
-          {
-            label: 'JSON Web Key Sets',
-            link: '/reference/concepts/jwks',
-          },
-          {
-            label: 'Normalized user profile',
-            link: '/reference/concepts/normalized-user-profile',
-          },
-          {
-            label: 'Pagination',
-            link: '/reference/concepts/pagination',
-          },
-        ],
+        label: 'API Authentication',
+        link: '/reference/api-catalog/authenticate-scalekit-api',
       },
-      { label: 'SDKs', autogenerate: { directory: 'reference/sdks' } },
+      {
+        label: 'User authentication',
+        link: '/reference/concepts/auth-endpoints',
+      },
+      {
+        label: 'UI events',
+        link: '/reference/api-catalog/ui-events',
+      },
+      'home/guides/idtoken-claims',
       {
         label: 'Webhooks',
         autogenerate: { directory: 'reference/webhooks' },
       },
       {
         label: 'Errors',
-        collapsed: false,
         autogenerate: { directory: 'reference/errors' },
       },
     ],
   },
   {
-    label: 'Integrations',
+    label: 'Guides',
     link: '/integrations/',
     id: 'integrations',
-    icon: 'puzzle',
+    icon: 'open-book',
     items: [
       {
+        label: 'Concepts',
+        items: [
+          {
+            label: 'How to',
+            items: [
+              'home/guides/custom-domain',
+              'home/guides/idp-init-sso',
+              'home/guides/okta-sso-test',
+            ],
+          },
+          {
+            label: 'Basics',
+            items: ['home/guides/sso-basics'],
+          },
+        ],
+      },
+      {
+        label: 'Administrator guides',
+        items: [
+          {
+            label: 'SSO Integrations',
+            autogenerate: { directory: 'integrations/sso-integrations' },
+          },
+          {
+            label: 'SCIM Integrations',
+            autogenerate: { directory: 'integrations/scim-integrations' },
+          },
+        ],
+      },
+      {
         label: 'Social Connections',
-        collapsed: true,
         autogenerate: { directory: 'integrations/social-connections' },
       },
       {
-        label: 'SSO Integrations',
-        collapsed: true,
-        autogenerate: { directory: 'integrations/sso-integrations' },
-      },
-      {
-        label: 'SCIM Integrations',
-        collapsed: true,
-        autogenerate: { directory: 'integrations/scim-integrations' },
-      },
-      {
         label: 'Auth Systems',
-        collapsed: true,
         autogenerate: { directory: 'integrations/auth-systems' },
       },
     ],
