e79a26a7c220e3f04ddbebc3e1c9b85303bc0c68
saif-at-scalekit <saif.shaik@scalekit.com>
2025-05-26 14:47:40 +0530

    Update redirects configuration to enhance navigation
    - Added new redirects for best practices and integration paths, improving user access to relevant guides.
    - Revised existing redirects for clarity and organization, ensuring a more intuitive navigation experience.

--- src/configs/redirects.config.ts
+++ src/configs/redirects.config.ts
@@ -12,17 +12,27 @@ export const redirects = {
   '/sso/guides/key-concepts/authorization-url':
     '/guides/sso/authorization-url/',
   '/sso/guides/key-concepts/redirect-uri': '/reference/redirects/',
+  '/best-practices/redirect-uri': '/reference/redirects/',
   '/sso/guides/key-concepts/idtoken-claims': '/guides/idtoken-claims/',
   '/sso/guides/key-concepts/manage-client-secrets':
     '/guides/client-credentials-practices/',
+  '/best-practices/manage-client-secrets':
+    '/guides/client-credentials-practices/',
   '/sso/guides/key-concepts/user-profile': '/guides/sso/user-profile-details/',
   '/sso/guides/test-your-integration/using-okta': '/guides/sso/okta-sso-test/',
+  '/sso/overview': '/guides/sso/sso-basics/',
 
   // SCIM
   '/scim/checklist': '/guides/directory/admin-portal/',
 
   // integrations
-  '/integrations/[...slug]/': '/guides/integrations/auth-systems/[...slug]/',
+  '/integrations': '/guides/integrations/sso-integrations/',
+
+  // auth systems
+  '/integrations/auth0/': '/guides/integrations/auth-systems/auth0/',
+  '/integrations/firebase/': '/guides/integrations/auth-systems/firebase/',
+  '/integrations/cognito/': '/guides/integrations/auth-systems/aws-cognito/',
+
   '/integrations/azure-ad-saml':
     '/guides/integrations/sso-integrations/azure-ad-saml/',
   '/integrations/okta-saml': '/guides/integrations/sso-integrations/okta-saml/',
@@ -30,18 +40,30 @@ export const redirects = {
     '/guides/integrations/sso-integrations/onelogin-saml/',
   '/integrations/jumpcloud':
     '/guides/integrations/sso-integrations/jumpcloud-saml/',
-  '/integrations/azure-scim':
-    '/guides/integrations/scim-integrations/azure-scim/',
-  '/integrations/okta-scim':
-    '/guides/integrations/scim-integrations/okta-scim/',
   '/integrations/onelogin': '/guides/integrations/scim-integrations/onelogin/',
-  '/integrations/scim-connections/[...slug]':
-    '/guides/integrations/scim-integrations/[...slug]/',
+  '/integrations/azure-scim/':
+    '/guides/integrations/scim-integrations/azure-scim/',
+  '/integrations/okta-scim/':
+    '/guides/integrations/scim-integrations/okta-scim/',
+  '/integrations/scim-connections/google-dir-sync/':
+    '/guides/integrations/scim-integrations/google-dir-sync/',
+  '/integrations/scim-connections/jumpcloud/':
+    '/guides/integrations/scim-integrations/jumpcloud/',
 
   // social logins
   '/social-logins': '/social-logins/quickstart/',
-  '/integrations/social-connections/[...slug]/':
-    '/guides/integrations/social-connections/[...slug]/',
+  '/integrations/social-connections/google/':
+    '/guides/integrations/social-connections/google/',
+  '/integrations/social-connections/microsoft/':
+    '/guides/integrations/social-connections/microsoft/',
+  '/integrations/social-connections/github/':
+    '/guides/integrations/social-connections/github/',
+  '/integrations/social-connections/gitlab/':
+    '/guides/integrations/social-connections/gitlab/',
+  '/integrations/social-connections/linkedin/':
+    '/guides/integrations/social-connections/linkedin/',
+  '/integrations/social-connections/salesforce/':
+    '/guides/integrations/social-connections/salesforce/',
 
   '/sdks': '/dev-kit/',
   '/api/webhook-events': '/dev-kit/webhooks/directory-events/',
