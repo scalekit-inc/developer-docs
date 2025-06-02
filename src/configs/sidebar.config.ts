b09c1e91061e893b530a1d102e7a1a162352d0ec
saif-at-scalekit <saif.shaik@scalekit.com>
2025-06-02 19:49:32 +0530

    Update sidebar configuration to comment out custom email templates and mark it as a draft; enhance custom email provider documentation with support contact information.

--- src/configs/sidebar.config.ts
+++ src/configs/sidebar.config.ts
@@ -103,7 +103,7 @@ export const sidebar = [
         label: 'Email Customization',
         items: [
           'guides/passwordless/custom-email-provider',
-          'guides/passwordless/custom-email-templates',
+          // 'guides/passwordless/custom-email-templates',
         ],
       },
     ],
