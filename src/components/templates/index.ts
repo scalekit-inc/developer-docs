251870122c3d2f00ef368ba430540a6cd18878cb
saif-at-scalekit <saif.shaik@scalekit.com>
2025-06-14 16:03:48 +0530

    Update signup documentation: add CreateUserMembershipSection to templates and enhance import statements for improved clarity and functionality.

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -3,3 +3,4 @@ export { default as EnvSection } from './_env.mdx';
 export { default as RedirectAuthPageSection } from './_user-authpage.mdx';
 export { default as RetrieveUserDetailsSection } from './_retrieve-user-details.mdx';
 export { default as UserProfileSection } from './_user-profile.mdx';
+export { default as CreateUserMembershipSection } from './_create-user-membership.mdx';
