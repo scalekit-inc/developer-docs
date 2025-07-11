58785897db112d72891cbfdc517256cbc674fb9b
saif-at-scalekit <saif.shaik@scalekit.com>
2025-07-11 11:51:59 +0530

    fix the redirections links

--- src/configs/redirects.config.ts
+++ src/configs/redirects.config.ts
@@ -1,35 +1,34 @@
 export const redirects = {
   // SSO
   '/sso/quickstart-admin-portal': '/guides/sso/admin-portal',
+  '/scim/quickstart': '/directory/scim/quickstart/',
+  '/reference/errors': '/reference/sso/sso-integration-errors/',
+  '/sso/guides/integrate-with-your-auth-system/firebase':
+    '/guides/integrations/auth-systems/firebase/',
+  '/sso/social-login': '/social-logins/quickstart/',
+  '/sso/social-logins-quickstart': '/social-logins/quickstart/',
 
   // SSO Guides
-  '/sso/guides/setup-sso/implement-idp-initiated-sso':
-    '/guides/sso/idp-init-sso/',
-  '/sso/guides/setup-sso/handle-user-profile':
-    '/guides/sso/sso-user-attributes/',
+  '/sso/guides/setup-sso/implement-idp-initiated-sso': '/guides/sso/idp-init-sso/',
+  '/sso/guides/setup-sso/handle-user-profile': '/guides/sso/sso-user-attributes/',
   '/sso/guides/setup-sso/build-login-page': '/sso/quickstart/',
-  '/sso/guides/key-concepts/authorization-url':
-    '/guides/sso/authorization-url/',
+  '/sso/guides/key-concepts/authorization-url': '/guides/sso/authorization-url/',
   '/sso/guides/key-concepts/redirect-uri': '/reference/redirects/',
   '/best-practices/redirect-uri': '/reference/redirects/',
   '/sso/guides/key-concepts/idtoken-claims': '/guides/idtoken-claims/',
-  '/sso/guides/key-concepts/manage-client-secrets':
-    '/guides/client-credentials-practices/',
-  '/best-practices/manage-client-secrets':
-    '/guides/client-credentials-practices/',
+  '/sso/guides/key-concepts/manage-client-secrets': '/guides/client-credentials-practices/',
+  '/best-practices/manage-client-secrets': '/guides/client-credentials-practices/',
   '/sso/guides/key-concepts/user-profile': '/guides/sso/user-profile-details/',
   '/sso/guides/test-your-integration/using-okta': '/guides/sso/okta-sso-test/',
   '/sso/overview': '/guides/sso/sso-basics/',
-  '/sso/guides/setup-sso/customize-user-attributes':
-    '/guides/sso/sso-user-attributes/',
+  '/sso/guides/setup-sso/customize-user-attributes': '/guides/sso/sso-user-attributes/',
 
   // SCIM
   '/scim/checklist': '/guides/directory/admin-portal/',
   '/scim/basics': '/guides/directory/user-provisioning-basics/',
   '/scim/protocol': '/guides/directory/scim-protocol/',
   '/scim/webhooks': '/reference/webhooks/directory-events/',
-  '/scim/automatically-assign-roles':
-    '/guides/directory/group-based-role-assignment/',
+  '/scim/automatically-assign-roles': '/guides/directory/group-based-role-assignment/',
   '/scim/map-user-attributes': '/guides/directory/user-provisioning-basics/',
   '/scim/listen-directory-events': '/reference/webhooks/directory-events/',
   '/scim/gbac': '/guides/directory/group-based-role-assignment/',
@@ -49,56 +48,42 @@ export const redirects = {
   '/integrations': '/guides/integrations/',
   '/integrations/saml': '/guides/integrations/sso-integrations/generic-saml/',
   '/integrations/oidc': '/guides/integrations/sso-integrations/generic-oidc/',
-  '/integrations/adfs-saml':
-    '/guides/integrations/sso-integrations/microsoft-ad-fs/',
+  '/integrations/adfs-saml': '/guides/integrations/sso-integrations/microsoft-ad-fs/',
 
   // auth systems
   '/integrations/auth0/': '/guides/integrations/auth-systems/auth0/',
   '/integrations/firebase/': '/guides/integrations/auth-systems/firebase/',
   '/integrations/cognito/': '/guides/integrations/auth-systems/aws-cognito/',
 
-  '/integrations/azure-ad-saml':
-    '/guides/integrations/sso-integrations/azure-ad-saml/',
+  '/integrations/azure-ad-saml': '/guides/integrations/sso-integrations/azure-ad-saml/',
   '/integrations/okta-saml': '/guides/integrations/sso-integrations/okta-saml/',
-  '/integrations/onelogin-saml':
-    '/guides/integrations/sso-integrations/onelogin-saml/',
-  '/integrations/jumpcloud':
-    '/guides/integrations/sso-integrations/jumpcloud-saml/',
+  '/integrations/onelogin-saml': '/guides/integrations/sso-integrations/onelogin-saml/',
+  '/integrations/jumpcloud': '/guides/integrations/sso-integrations/jumpcloud-saml/',
   '/integrations/onelogin': '/guides/integrations/scim-integrations/onelogin/',
-  '/integrations/azure-scim/':
-    '/guides/integrations/scim-integrations/azure-scim/',
-  '/integrations/okta-scim/':
-    '/guides/integrations/scim-integrations/okta-scim/',
-  '/integrations/onelogin-scim':
-    '/guides/integrations/scim-integrations/onelogin/',
+  '/integrations/azure-scim/': '/guides/integrations/scim-integrations/azure-scim/',
+  '/integrations/okta-scim/': '/guides/integrations/scim-integrations/okta-scim/',
+  '/integrations/onelogin-scim': '/guides/integrations/scim-integrations/onelogin/',
   '/integrations/scim-connections/google-dir-sync/':
     '/guides/integrations/scim-integrations/google-dir-sync/',
-  '/integrations/scim-connections/jumpcloud/':
-    '/guides/integrations/scim-integrations/jumpcloud/',
-  '/integrations/google-saml':
-    '/guides/integrations/sso-integrations/google-saml/',
-  '/integrations/pingidentity-saml':
-    '/guides/integrations/sso-integrations/pingidentity-saml/',
-  '/integrations/jumpcloud-saml':
-    '/guides/integrations/sso-integrations/jumpcloud-saml/',
+  '/integrations/scim-connections/jumpcloud/': '/guides/integrations/scim-integrations/jumpcloud/',
+  '/integrations/google-saml': '/guides/integrations/sso-integrations/google-saml/',
+  '/integrations/pingidentity-saml': '/guides/integrations/sso-integrations/pingidentity-saml/',
+  '/integrations/jumpcloud-saml': '/guides/integrations/sso-integrations/jumpcloud-saml/',
 
   // social logins
   '/social-logins': '/social-logins/quickstart/',
-  '/integrations/social-connections/google/':
-    '/guides/integrations/social-connections/google/',
+  '/integrations/social-connections/google/': '/guides/integrations/social-connections/google/',
   '/integrations/social-connections/microsoft/':
     '/guides/integrations/social-connections/microsoft/',
-  '/integrations/social-connections/github/':
-    '/guides/integrations/social-connections/github/',
-  '/integrations/social-connections/gitlab/':
-    '/guides/integrations/social-connections/gitlab/',
-  '/integrations/social-connections/linkedin/':
-    '/guides/integrations/social-connections/linkedin/',
+  '/integrations/social-connections/github/': '/guides/integrations/social-connections/github/',
+  '/integrations/social-connections/gitlab/': '/guides/integrations/social-connections/gitlab/',
+  '/integrations/social-connections/linkedin/': '/guides/integrations/social-connections/linkedin/',
   '/integrations/social-connections/salesforce/':
     '/guides/integrations/social-connections/salesforce/',
 
   // Other redirects
   '/sdks': '/dev-kit/',
+  '/dev-kit/webhooks/event-object/': '/dev-kit',
   '/api/webhook-events': '/reference/webhooks/directory-events/',
   '/manage-scalekit/glossary': '/reference/glossary/',
   '/index-old': '/',
@@ -107,4 +92,4 @@ export const redirects = {
   '/sso/domain-and-theme-customization': '/guides/custom-domain/',
   '/sso/bkp/quickstart-admin-portal': '/guides/sso/admin-portal/',
   '/contact-us': '/support/contact-us/',
-};
+}
