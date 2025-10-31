e251cbbee437526c23a5cf52f022cb31b488977f
Saif Ali Shaik <saif.shaik@scalekit.com>
2025-10-31 20:03:40 +0530

    feat: add delete users and organizations documentation
    - Introduced a new guide for deleting users and organizations from accounts, enhancing the user management documentation within Scalekit.
    - Updated the sidebar configuration to include the new guide, improving navigation for users seeking information on user removal processes.
    
    This update aims to provide comprehensive instructions for managing user and organization deletions, ensuring clarity and usability for developers.

--- src/configs/sidebar.config.ts
+++ src/configs/sidebar.config.ts
@@ -37,6 +37,7 @@ export const sidebar = [
           'authenticate/manage-organizations/add-users-to-organization',
           'fsa/guides/custom-user-attributes',
           'authenticate/manage-organizations/remove-users-from-organization',
+          'authenticate/manage-users-orgs/delete-users-orgs',
         ],
       },
       {
