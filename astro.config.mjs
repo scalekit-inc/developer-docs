3067c7d425930ded0272d3e0f86a14af2a28d11c
Saif Ali Shaik <saif.shaik@scalekit.com>
2026-01-21 19:15:33 +0530

    Add prefix for cookbooks in Astro configuration
    - Updated the Astro configuration to include a prefix for the starlight-blog integration, enhancing the organization of blog content under the 'cookbooks' category. This change aims to improve navigation and content discoverability for users.

--- astro.config.mjs
+++ astro.config.mjs
@@ -96,6 +96,7 @@ export default defineConfig({
           hideMainActionLabel: true,
         }),
         starlightBlog({
+          prefix: 'cookbooks',
           metrics: {
             readingTime: true,
             words: 'total',
