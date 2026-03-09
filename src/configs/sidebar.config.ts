d7fb9d12c1b4eb7656a6189b5c7a407ff66323df
Saif Ali Shaik <saif.shaik@scalekit.com>
2026-03-09 15:31:52 +0530

    update the context7 guide to help with develoepr resources

--- src/configs/sidebar.config.ts
+++ src/configs/sidebar.config.ts
@@ -255,19 +255,7 @@ export const sidebar = [
         link: '/apis/#description/overview',
         attrs: { target: '_blank', rel: 'noopener noreferrer', class: 'external-link' },
       },
-      {
-        label: 'API collections',
-        collapsed: false,
-        items: [
-          // 'dev-kit/api-collections/postman-collections', TODO: Expand the guide for postman collections
-          {
-            label: 'Postman collections',
-            link: 'https://github.com/scalekit-inc/api-collections/tree/main/postman',
-            attrs: { target: '_blank', rel: 'noopener noreferrer', class: 'external-link' },
-          },
-          'dev-kit/api-collections/openapi-spec',
-        ],
-      },
+      'dev-kit/api-collections/openapi-spec',
       {
         label: 'AI tools',
         collapsed: false,
