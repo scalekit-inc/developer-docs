74df0776ca439e5d797424b99e330651c57b75d5
Saif Ali Shaik <saif.shaik@scalekit.com>
2026-03-15 12:42:18 +0530

    Decrease NODE memory limit and disable OG prerendering

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -11,7 +11,7 @@ const entries = await getCollection('docs')
 // Map entries to an object keyed by id for quick lookup
 const pages = Object.fromEntries(entries.map(({ id, data }) => [id, { data }]))
 
-export const prerender = true
+export const prerender = false
 
 // Export the `GET` handler and static paths generator expected by Astro
 export const { getStaticPaths, GET } = await OGImageRoute({
