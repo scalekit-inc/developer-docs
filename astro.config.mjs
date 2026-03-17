95409c01b3ded4123c498459a6e0080b877d1ce8
Saif Ali Shaik <saif.shaik@scalekit.com>
2026-03-17 20:47:27 +0530

    change output to server mode in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   site: 'https://docs.scalekit.com',
   redirects,
   integrations: [
