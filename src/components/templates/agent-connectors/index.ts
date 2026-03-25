db3ca0e0d45cd113d261f602eba655b98b371a86
Pranesh <praneshtaker@gmail.com>
2026-03-25 20:42:53 +0530

    Revert "feat(harvestapi): add HarvestAPI connector doc with harvest.svg icon and full tool list"
    This reverts commit 5c49c501da34ae8b835a07ececba3729090f393b.

--- src/components/templates/agent-connectors/index.ts
+++ src/components/templates/agent-connectors/index.ts
@@ -1,3 +1,1 @@
 export { default as SetupPipedriveSection } from './_setup-pipedrive.mdx'
-export { default as SetupHarvestapiSection } from './_setup-harvestapi.mdx'
-export { default as UsageHarvestapiSection } from './_usage-harvestapi.mdx'
