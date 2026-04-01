43b5dde017fc82a7ffd95e61f42696e48823e11f
Pranesh <praneshtaker@gmail.com>
2026-04-01 17:15:07 +0530

    fix: add Twitter connector setup assets and resolve CodeRabbit issues

--- ec.config.mjs
+++
@@ -1,9 +0,0 @@
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
-import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
-
-/** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
-
-export default {
-  plugins: [pluginLineNumbers(), pluginCollapsibleSections()],
-  defaultProps: { collapseStyle: 'collapsible-auto' },
-};
